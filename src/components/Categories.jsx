2a32ebea086c892a0ce826c0869b92c86fe3ba08
Alexandr Kulish <81294507+Daryk-less-than-3@users.noreply.github.com>
2022-08-17 16:14:59 +0300

    add rendering categories and toggle active categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
+
 function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Острые', 'Вегетарианская', 'Гриль', 'Закрытые'];
+
+  const onClickCategory = (a) => {
+    setActiveCategory(a);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeCategory === index ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
